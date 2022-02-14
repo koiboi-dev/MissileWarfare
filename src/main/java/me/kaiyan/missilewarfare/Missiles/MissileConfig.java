2a02a010a81adf2a81ae2d36a8f7fb6eb6f664b3
koiboi-dev <63368980+koiboi-dev@users.noreply.github.com>
2022-02-14 21:30:56 +0400

    Fixed fixed fixed it

--- src/main/java/me/kaiyan/missilewarfare/Missiles/MissileConfig.java
+++ src/main/java/me/kaiyan/missilewarfare/Missiles/MissileConfig.java
@@ -11,7 +11,7 @@ public class MissileConfig {
 
     public static MissileClass[] missiles;
 
-    public void setup(Config cfg){
+    public static void setup(Config cfg){
         List<MissileClass> outMissiles = new ArrayList<>();
         for (String key : cfg.getKeys("missiles")){
             outMissiles.add(new MissileClass(cfg.getDouble("missiles."+key+".speed"), cfg.getInt("missiles."+key+".range"), cfg.getInt("missiles."+key+".power"), cfg.getInt("missiles."+key+".accuracy"), VariantsAPI.getIntTypeFromSlimefunitemID(key)));
